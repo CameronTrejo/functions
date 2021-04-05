5efbb363c09f8d8347d994f9f9db61548d5c5ed5
CameronTrejo2000 <camerontrejo2000@gmail.com>
2021-04-05 06:54:29 -0600

    Initial conversion calculations

--- calculator.py
+++ calculator.py
@@ -2,8 +2,16 @@
 # TODO User input validation
 userInput = input('What is the number: ')
 print('Your number is:', userInput) # TODO remove for prod
-userNumber = int(userInput) 
-
+userNumber = float(userInput) 
 
 # Perform the conversion
+# Convert in to mm 25.4 in every 1 in
+
+# Convert from in to mm (userNumber * 25.4) (1 * 25.4) = 25.4 mm
+userAnswer = userNumber * 25.4
+
+# Convert from mm to in (userNumber/ 25.4 ) 1/25.4 = 0.394
+userAnswer = userNumber/ 25.4
+
 # Print that information out. The answer
+print('The answer is:', userAnswer)
